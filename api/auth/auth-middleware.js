d1cd822194d249585a94c1913e2fad074f744305
Marlon Colato <mcolato2104@gmail.com>
2024-03-02 14:19:34 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -44,7 +44,11 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
- next()
+    if(role_name ===  req.decodedToken.role_name ){
+      next()
+    } else {
+      next({ status: 403, message: 'This is not for you' })
+    }
 }
 
 
@@ -59,7 +63,7 @@ const checkUsernameExists = async (req, res, next) => {
  try{
   const [user] = await User.findBy({ username: req.body.username })
   if(!user) {
-    next({status: 422, message: 'Invalid credentials'})
+    next({status: 401, message: 'Invalid credentials'})
   } else {
     req.user = user
     next()
